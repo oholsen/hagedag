229895a6d90fb27ae4b84360d87afa383ce27cdb
Øystein Haug Olsen <oo@tellu.no>
2019-08-17 22:43:10 +0200

    Cleanup

--- control/video.py
+++ control/video.py
@@ -74,154 +74,91 @@ def main():
             p1 = tuple(map(int, pts.pop(0)))
             cv2.line(frame, p0, p1, (0, 255, 0), 2)
             p0 = p1
-        #cv2.rectangle(frame, (580, 350), (1200, 600), (0, 255, 0), 2)
 
         cv2.imshow('frame', frame)
+
         if course is None:
-            # logger.info("Set course background %r", frame)
-            course = frame.copy() #cv2.copyMakeBorder(frame,0,0,0,0,cv2.BORDER_REPLICATE)
+            course = frame.copy()
             cv2.imshow('course', course)
 
-        if 0:
-            # Our operations on the frame come here
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            cv2.imshow('gray', gray)
+        blur = cv2.GaussianBlur(frame, (21, 21), 0)
+        # cv2.imshow('blur', blur)
+        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)    
+        # Threshold the HSV image to get only white colors
+        mask = cv2.inRange(hsv, lower_white, upper_white)
+        #cv2.imshow('mask',mask)
+        #res = cv2.bitwise_and(frame, frame, mask=mask)
+        #cv2.imshow('res',res)
 
-            blur = cv2.GaussianBlur(gray, (21, 21), 0)
-            cv2.imshow('blur', blur)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) == 0:
+            logger.info("No contours")
+            continue
+        # the contours are drawn here
+        #cv2.drawContours(mask, contours, -1, 255, 3)
+        #cv2.imshow("mask", mask)
+        # find the contour with the biggest area
+        c = max(contours, key = cv2.contourArea)
+        M = cv2.moments(c)
 
+        try:
+            cX, cY = centroid(M)
+        except:
+            logger.info("No point")
+            continue
 
-            #th = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
-            #cv2.imshow('Adaptive threshold', th)
-            retval, th2 = cv2.threshold(blur, 230, 255, cv2.THRESH_BINARY)
-            cv2.imshow('Binary threshold', th2)
+        p = Point(cX, cY)
+        A = area(M)
 
-
-            opening = cv2.morphologyEx(th2, cv2.MORPH_OPEN, kernel)
-            cv2.imshow('opening', opening)
-
-            if 0:
-                hist = cv2.calcHist([gray],[0],None,[256],[0,256])
-                print(hist)
-                plt.plot(hist)
-                plt.show()
-
-        if 0:
-            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-
-            if ids is not None:
-                for c, i in zip(corners, ids):
-                    print(i, c, flush=True)
-
-            print(corners, ids, flush=True)
-            gray = aruco.drawDetectedMarkers(gray, corners)
-            cv2.imshow('frame', gray)
-
-        if 1:
-            blur = cv2.GaussianBlur(frame, (21, 21), 0)
-            # cv2.imshow('blur', blur)
-
-            hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)    
-            if 0:
-                hist = cv2.calcHist( [hsv], [0, 1], None, [180, 256], [0, 180, 0, 256] )
-                plt.imshow(hist, interpolation = 'nearest')
-                plt.show()
-
-            # Threshold the HSV image to get only white colors
-            mask = cv2.inRange(hsv, lower_white, upper_white)
-            cv2.imshow('mask',mask)
-            #res = cv2.bitwise_and(frame, frame, mask=mask)
-            #cv2.imshow('res',res)
-
-            if 1:
-                contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                #print(contours)
-                #print(hierarchy)
-                if len(contours) == 0:
-                    logger.info("No contours")
-                    continue
-                # the contours are drawn here
-                cv2.drawContours(mask, contours, -1, 255, 3)
-                cv2.imshow("mask", mask)
-                # find the contour with the biggest area
-                c = max(contours, key = cv2.contourArea)
-                M = cv2.moments(c)
-
-                #x,y,w,h = cv2.boundingRect(c)
-                #print("bound", x,y,w,h)
-                #output = frame.copy()
-                #cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
-                #cv2.imshow("Contours", output)
-
-            else:
-                # calculate moments of binary image
-                M = cv2.moments(mask)
-                #logger.info("Moments %r", M)
-                logger.info("Moments %.2f  %.2f", M["nu02"], M["nu20"])
-                if not M["m00"]:
-                    logger.info("No point")
-                    # Stop robot after some timeout....
-                    continue
-
-            # calculate x,y coordinate of center
-            try:
-                cX, cY = centroid(M)
-            except:
-                logger.info("No point")
-                continue
-
-            p = Point(cX, cY)
-            A = area(M)
-
-            if not (A_min <= A <= A_max):
-                logger.info("Invalid area %d %d %r %.2f %.2f", cX, cY, fence.contains(p), fence.exterior.distance(p), A)
-                cv2.circle(frame, (cX, cY), 25, (255, 255, 0), 2)
-                cv2.imshow("frame", frame)
-                continue
-
-            cv2.circle(frame, (cX, cY), 25, (0, 0, 0), 2)
+        if not (A_min <= A <= A_max):
+            logger.info("Invalid area %d %d %r %.2f %.2f", cX, cY, fence.contains(p), fence.exterior.distance(p), A)
+            cv2.circle(frame, (cX, cY), 25, (255, 255, 0), 2)
             cv2.imshow("frame", frame)
+            continue
 
-            logger.info("point %d %d %r %.2f %.2f", cX, cY, fence.contains(p), fence.exterior.distance(p), A)
+        cv2.circle(frame, (cX, cY), 25, (0, 0, 0), 2)
+        cv2.imshow("frame", frame)
 
-            new_point = (cX, cY)
-            if last_point:
-                cv2.line(course, last_point, new_point, (10,10,10), 2)
-            last_point = new_point
-            cv2.imshow("course", course)
+        logger.info("Point %d %d %r %.2f %.2f", cX, cY, fence.contains(p), fence.exterior.distance(p), A)
 
-            # Buffer zone outside the fence: will run avoidance maneouvre.
-            # Get into an outside state, if staying outside too long, then abort
-            if not fence.contains(p):
+        new_point = (cX, cY)
+        if last_point:
+            cv2.line(course, last_point, new_point, (10,10,10), 2)
+        last_point = new_point
+        cv2.imshow("course", course)
 
-                if not outside:
-                    logger.info("OUTSIDE")
-                    outside = True
-                    robot.avoid(speed, turn)
-                    # Only tries it once! So don't have to wait for it to complete.
-                    # Will only continue when if the avoid ends up inside
-                    continue
+        # Buffer zone outside the fence: will run avoidance maneouvre.
+        # Get into an outside state, if staying outside too long, then abort
+        if not fence.contains(p):
 
-                # TODO: check if avoidance is fininshed...
+            if not outside:
+                logger.info("OUTSIDE")
+                outside = True
+                robot.avoid(speed, turn)
+                # Only tries it once! So don't have to wait for it to complete.
+                # Will only continue when if the avoid ends up inside
                 continue
 
-                assert outside
-                if fence.exterior.distance(p) < fence_distance:
-                    robot.avoid(speed, turn)
-                    # Only tries it once! So don't have to wait for it to complete.
-                    # Will only continue when if the avoid ends up inside
-                    continue
+            # TODO: check if avoidance is fininshed...
+            continue
 
+            assert outside
+            if fence.exterior.distance(p) < fence_distance:
+                robot.avoid(speed, turn)
+                # Only tries it once! So don't have to wait for it to complete.
+                # Will only continue when if the avoid ends up inside
                 continue
 
-            # inside fence
-            if outside:
-                logger.info("INSIDE")
-                #robot.send(robot.Speed(speed))
-                outside = False
-                #continue
+            continue
 
-            robot.send("heartbeat")
+        # inside fence
+        if outside:
+            logger.info("INSIDE")
+            #robot.send(robot.Speed(speed))
+            outside = False
+            #continue
+
+        robot.send("heartbeat")
 
 
 
