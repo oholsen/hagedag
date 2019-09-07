80b526ce8c33ed7df53ade8d292457f6f314cc72
Øystein Haug Olsen <oo@tellu.no>
2019-09-07 10:10:06 +0200

    Add Cut0 button, also stop STM on RPi heartbeat timeout

--- raspberry/control/control.py
+++ raspberry/control/control.py
@@ -5,21 +5,26 @@ import servo
 import signal
 
 
-
-
-
 pi = pigpio.pi()
 pi.set_mode(14, pigpio.ALT5) # TXD
 pi.set_mode(15, pigpio.ALT5) # RXD
 
 stm = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=3.0)
 cutter = servo.cutter
+# Arm the motor controller by giving PWM signal
 cutter.power(0)
 
 
-def handler(signum, frame):
-    print('Heartbeat timeout with signal', signum)
+def stop():
     cutter.off()
+    stm.write('.' + '\n')
+
+
+def handler(signum, frame):
+    # also heartbeat timeout inside STM
+    print('Heartbeat timeout with signal', signum)
+    stop()
+
 
 # Set the signal handler 
 signal.signal(signal.SIGALRM, handler)
@@ -29,14 +34,16 @@ def handle(data):
     # Dispatch message to RPI or STM
     data = data.strip()
 
-    # TODO: Watchdog on the cutter, if loose touch then stop...
+    # Watchdog on the cutter, if loose touch then stop...
     if data.startswith('heartbeat'):
         # reset timer, pass on to STM
         signal.alarm(5) # seconds
+        # forward to STM
 
-    elif data == '!' or data == '.':
-        cutter.off()
-    
+    elif data == 'STOP':
+        stop()
+        return
+
     elif data.startswith('CUT'):
         try:
             cols = data.split()
@@ -46,5 +53,8 @@ def handle(data):
             print("Failed to handle CUT: " + data)
         return
 
+    elif data == '!' or data == '.':
+        cutter.off()
+        # forward to STM
+    
     stm.write(data + '\n')
-
