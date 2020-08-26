b055db8d251180fa41ae56a3401d0e3d6881a7ad
Øystein Haug Olsen <oholsen@gmail.com>
2020-08-26 22:30:58 +0200

    Cleanup and doc, include some thinking around holes

--- control/geometry.py
+++ control/geometry.py
@@ -5,24 +5,36 @@ import math
 
 
 def path(p0, p1, fence) -> LineString:
+
+    # FIXME: support holes too - so fence is really an area/shape that is navigable
     coords = fence.exterior.coords
+    
+    # make sure the contains predicate is true also for points on the exterior.
+    # alternatively, could shrink/erode area in order to get candidate corners!
+    # anyway, better not to cut the corners too tight anyway. but risk that p0 or p1
+    # is outside the eroded area.
     fence = fence.buffer(0.01)
+
+    # check for direct path
     line = LineString([p0, p1])
     if fence.contains(line):
         return line
 
-    paths = {} #  coord -> shortest path from p0
-    queue = []
+    paths = {} #  corner -> shortest path from p0
+    queue = [] #  corners to be processed
 
-    # lines from p0 to every corner
+    # init with direct line from p0 to every corner
     for c in coords:
         line = LineString([p0, c])
         if fence.contains(line):
             paths[c] = line
             queue.append(c)
 
-    # find shortest path to every corner
+    # find shortest path from p0 to every corner.
+    # not every corner is directly accessible from p0.
     while queue:
+        # we have already found a shortest path from p0 to c0.
+        # check if c0 can be in the shortest path to other corners.
         c0 = queue.pop(0)
         for c in coords:
             if c == c0:
@@ -30,20 +42,17 @@ def path(p0, p1, fence) -> LineString:
             line = LineString([c0, c])            
             if not fence.contains(line):
                 continue
-            path0 = paths[c0]
-            path1 = paths.get(c)
-            if path1 is None:
-                path1b = linemerge([path0, line])
-            else:
-                # Two paths to c: path1 and path0 + line
-                path1b = linemerge([path0, line])
-                if path1b.length > path1.length:
-                    path1b = None
-            if path1b:
-                # will enque while finding shorter paths
+            # Two paths to c: paths[c] and paths[c0] + line(c0,c)
+            path1a = paths.get(c)
+            path1b = linemerge([paths[c0], line])
+            if path1a is None or path1b.length < path1a.length:
+                # path1b is shorter
                 assert fence.contains(path1b)
                 paths[c] = path1b
-                queue.append(c)
+                # Need to check again if there is a shorter path to c?
+                # There cannot possibly be one? The shortest path will be the fewest number of hops.
+                # The shortest path will be in this iteration over c, so no need to enqueue?
+                # queue.append(c)
 
     # find shortest path to destination
     dist = math.inf
@@ -63,7 +72,7 @@ def path(p0, p1, fence) -> LineString:
 if __name__ == "__main__":
     p = Polygon([(0,0), (10,0), (10,10), (5, 10), (5,5), (4,5), (4,10), (0,10)])    
     # L shape
-    #p = Polygon([(0,0), (10,0), (10,10), (5, 10), (5,5), (0,5)])    
+    # p = Polygon([(0,0), (10,0), (10,10), (5, 10), (5,5), (0,5)])    
     # p = Polygon([(0,0), (1,0), (1,1), (0,1)])    
     print(p)
     l = LineString([(0,0), (1,0)])
